1dffb35dd51dc1417619c367a5e0f240af99de20
Don Alvarez <89621920+MintChipXD@users.noreply.github.com>
2023-02-18 07:45:52 +0800

    visitor model ang visitor list controller

--- controllers/visitorController.js
+++ controllers/visitorController.js
@@ -1,7 +1,14 @@
+const Visitor = require("../models/Visitor");
+
 const visitor = async (req, res) => {
     res.render('pages/visitor', { title: 'Visitors', path:'visitors' });
 }
 const visitorForm = async (req, res) => {
+    try {
+        const visitors = await Visitor.find();
+    } catch (error) {
+        
+    }
     res.render('pages/visitorForm', { title: 'Visitors', path:'visitors' });
 }
 
---
+++ models/Visitor.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+
+const visitorSchema = new mongoose.Schema({
+    name : {
+        type: String,
+    },
+    fever: {
+        type:Boolean,
+        required: true
+    },
+    coughAndColds: {
+        type:Boolean,
+        required: true
+    },
+    bodyPain: {
+        type:Boolean,
+        required: true
+    },
+    soreThroat: {
+        type:Boolean,
+        required: true
+    },
+    headAche: {
+        type:Boolean,
+        required: true
+    },
+    diarrhea: {
+        type:Boolean,
+        required: true
+    },
+    lostOfTasteOrSmell: {
+        type:Boolean,
+        required: true
+    },
+    diffBreathing: {
+        type:Boolean,
+        required: true
+    },
+    exposedToCovid: {
+        type:Boolean,
+        required: true
+    },
+    traveledOutside: {
+        type:Boolean,
+        required: true
+    },
+    certifiedTrue : {
+        type:Boolean,
+        required:true
+    }
+}, {timestamp: true})
+
+module.exports = mongoose.model('Visitor', visitorSchema)
